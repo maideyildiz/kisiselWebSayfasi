71ca7f2bac77fc220ee9e1fd8ce46738460f3ec9
Maide Yıldız <maideyildizz@gmail.com>
2020-05-10 07:49:02 +0300

    Login Sayfasının Devamı

---
+++ login2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lan="tr">
+<head> 
+    
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="global.css">
+    <title> ~Maide YILDIZ~ </title>
+</head>
+    <body>
+<?php function kontrol($kullanici, $sifre){
+    $dogruKullanici="b191210310@gmail.com";
+    $dogruSifre="1234";
+  if($kullanici==$dogruKullanici && $sifre==$dogruSifre)
+{
+   echo "<p1>HOŞGELDİN B191210310!</p1>";
+}
+    else{  header("Location: login.php");}
+}            
+?>
+    <nav>
+	<ol>
+		<li><a href="hakkinda.html"> Hakkında </a></li>
+        <li><a href="cv.html"> CV </a></li>
+        <li><a href="benimSehrim.html"> Benim Şehrim </a></li>
+        <li><a href="mirasimiz.html"> Mirasımız </a></li>
+        <li><a href="iletisim.php"> İletişim </a></li>
+        <li><a id="login" href="login.php"><img src="pictures/login.png" id="logo"></a></li>
+	</ol>
+</nav>  
+        
+<section> 
+    <div>
+        <br>
+	<h3>GİRİŞ YAPTINIZ</h3>
+        <?php echo kontrol($_GET["mail"],$_GET["sifre"]);?>
+        </div>
+</section>
+    </body>
+
+
+</html>
