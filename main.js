05c04094ce250878b415979119caf40c4d8e8a5f
Maide Yıldız <maideyildizz@gmail.com>
2020-05-10 07:44:01 +0300

    Sayfanın Javascript Kodları

--- main.js
+++ main.js
@@ -1,17 +1,67 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+function kontrol(frm) 
+{
+    var adSoyad=frm.adSoyad.value;
+    var cinsiyet =frm.cinsiyet;
+    var mail = frm.mail.value;
+    var tel = frm.tel.value;
+    var yemek = frm.yemek;
+    var takim = frm.takim;
+    
+    var seciliMi = false;
+    var seciliMiYemek=false;
+    var seciliMiTakim=false;
+    
+    var atpos=mail.indexOf("@");
+    var dotpos=mail.lastIndexOf(".");
+ 
+    if ( adSoyad==null || adSoyad=="")
+    {
+        alert("Lütfen adınızı ve soyadınızı giriniz!");
+        return false;
+    }
+    
+    for ( var i = 0; i < cinsiyet.length; i++) {
+    if(cinsiyet[i].checked) {
+        seciliMi = true;
+        break;
+    }
+	}
+        if(!seciliMi)   
+    { 
+    alert("Lütfen cinsiyetinizi seçiniz!");
+        return false;
+    } 
+    
+    
+    
+    if ( atpos<1 || dotpos<atpos+2 || dotpos+2>=mail.length )
+    {
+        alert("Geçerli email adresi giriniz!");
+        return false;
+    }
+    
+    if(tel==null || tel=="" || tel.length<11)
+        {
+            alert("Lütfen geçerli bir telefon numarası giriniz!")
+            return false;
+        }
+    
+    for ( var i = 0; i < yemek.length; i++) {
+    if(yemek[i].checked) {
+        seciliMiYemek = true;
+        break;
+    }
+	}
+        if(!seciliMiYemek)   
+    { 
+    alert("Lütfen yemek seçiniz!");
+        return false;
+    }
+    
+    if(takim.selectedIndex<=0)   
+    { 
+       alert("Lütfen takımınızı seçiniz!");
+        return false;
+    }
+    return true;
 }
